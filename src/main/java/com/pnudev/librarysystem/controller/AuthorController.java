be293fc1b15df9767145d396f225adcedaa7e061
VladHolobyn <vlad.holobyn@gmail.com>
2023-04-12 23:17:15 +0300

    implemented deleting functionality added response statuses

--- src/main/java/com/pnudev/librarysystem/controller/AuthorController.java
+++ src/main/java/com/pnudev/librarysystem/controller/AuthorController.java
@@ -4,9 +4,13 @@ import com.pnudev.librarysystem.dto.AuthorDTO;
 import com.pnudev.librarysystem.service.AuthorService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
@@ -16,7 +20,14 @@ public class AuthorController {
     private final AuthorService authorService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public AuthorDTO addAuthor(@Valid @RequestBody AuthorDTO authorDTO){
         return authorService.addAuthor(authorDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAuthor(@PathVariable Long id){
+        authorService.deleteAuthor(id);
     }
 }
--- src/main/java/com/pnudev/librarysystem/service/AuthorService.java
+++ src/main/java/com/pnudev/librarysystem/service/AuthorService.java
@@ -1,8 +1,11 @@
 package com.pnudev.librarysystem.service;
 
 import com.pnudev.librarysystem.dto.AuthorDTO;
+import com.pnudev.librarysystem.entity.Author;
+import com.pnudev.librarysystem.exception.DeleteFailedException;
 import com.pnudev.librarysystem.mapper.AuthorMapper;
 import com.pnudev.librarysystem.repository.AuthorRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 
 import org.springframework.stereotype.Service;
@@ -16,5 +19,16 @@ public class AuthorService {
 
     public AuthorDTO addAuthor(AuthorDTO authorDTO) {
         return authorMapper.toDTO(authorRepository.save(authorMapper.toEntity(authorDTO)));
+    }
+
+    public void deleteAuthor(Long id) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Author with id: %d not found".formatted(id)));
+
+        if(!author.getBooks().isEmpty()){
+            throw new DeleteFailedException("Author with id: %d is used".formatted(id));
+        }
+
+        authorRepository.deleteById(id);
     }
 }
