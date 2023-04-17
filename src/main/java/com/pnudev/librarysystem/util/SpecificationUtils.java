c4e5c845d07a9c9a5530df5b6719c731730201aa
VladHolobyn <vlad.holobyn@gmail.com>
2023-04-17 14:37:21 +0300

    added @UtilityClass annotation

--- src/main/java/com/pnudev/librarysystem/util/SpecificationUtils.java
+++ src/main/java/com/pnudev/librarysystem/util/SpecificationUtils.java
@@ -1,9 +1,10 @@
 package com.pnudev.librarysystem.util;
 
+import lombok.experimental.UtilityClass;
 import org.springframework.data.jpa.domain.Specification;
 
+@UtilityClass
 public class SpecificationUtils{
-    private SpecificationUtils(){}
 
     public static <T> Specification<T> fieldContainsIgnoreCase(String field, String value){
         return (root, cq, cb) -> cb.like(cb.lower(root.get(field)), "%" + value.toLowerCase() +"%");
