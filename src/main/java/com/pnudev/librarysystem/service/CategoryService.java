052fd1896817f74dd5be0a481ac60ec63f006cf8
VladHolobyn <vlad.holobyn@gmail.com>
2023-04-26 22:32:35 +0300

    fixed remarks

--- src/main/java/com/pnudev/librarysystem/service/CategoryService.java
+++ src/main/java/com/pnudev/librarysystem/service/CategoryService.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-
 @Service
 @AllArgsConstructor
 public class CategoryService {
