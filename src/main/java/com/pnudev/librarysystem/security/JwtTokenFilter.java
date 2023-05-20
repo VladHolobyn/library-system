d4eeb0cb4bdfe2a7bddf138fbd5f50f8f36e174d
VladHolobyn <vlad.holobyn@gmail.com>
2023-05-20 23:29:40 +0300

    added custom Authentication

--- src/main/java/com/pnudev/librarysystem/security/JwtTokenFilter.java
+++ src/main/java/com/pnudev/librarysystem/security/JwtTokenFilter.java
@@ -6,10 +6,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -38,10 +36,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         String jwt = authHeader.substring(7);
         UserDetails userDetails = jwtService.extractUserDetails(jwt);
 
-        UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
+        JwtAuthenticationToken authentication = new JwtAuthenticationToken(jwt, userDetails);
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         filterChain.doFilter(request, response);
