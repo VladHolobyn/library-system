9a93d36cfa477cfbc5065cebcd14de40c21ed3c5
VladHolobyn <vlad.holobyn@gmail.com>
2023-05-21 00:11:20 +0300

    refactored exception handler

--- src/main/java/com/pnudev/librarysystem/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/pnudev/librarysystem/exception/handler/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ import com.pnudev.librarysystem.exception.NotUniqueException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,8 +22,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDTO handleValidationError(MethodArgumentNotValidException ex) {
-        List<String> errors = ex.getBindingResult().getFieldErrors()
+    public ErrorDTO handleValidationError(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getFieldErrors()
                 .stream().map(FieldError::getDefaultMessage).toList();
 
         return new ErrorDTO(errors);
@@ -32,44 +31,26 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorDTO handleEntityNotFoundException(EntityNotFoundException ex) {
-        return new ErrorDTO(List.of(ex.getMessage()));
+    public ErrorDTO handleEntityNotFoundException(EntityNotFoundException e) {
+        return new ErrorDTO(e.getMessage());
     }
 
-    @ExceptionHandler(OperationFailedException.class)
+    @ExceptionHandler({
+            OperationFailedException.class,
+            NotUniqueException.class,
+            BadCredentialsException.class,
+            EmptyFileException.class,
+            FileWrongTypeException.class
+    })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDTO handleOperationFailedException(OperationFailedException ex) {
-        return new ErrorDTO(List.of(ex.getMessage()));
+    public ErrorDTO handleBadRequestExceptions(RuntimeException e) {
+        return new ErrorDTO(e.getMessage());
     }
 
-    @ExceptionHandler(EmptyFileException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDTO handleEmptyFileException(EmptyFileException e) {
-        return new ErrorDTO(List.of(e.getMessage()));
-    }
-
-    @ExceptionHandler(FileWrongTypeException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDTO handleFileWrongTypeException(FileWrongTypeException e) {
-        return new ErrorDTO(List.of(e.getMessage()));
-    }
 
     @ExceptionHandler(IOErrorInFileException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorDTO handleIOErrorInFileException(IOErrorInFileException e) {
-        return new ErrorDTO(List.of(e.getMessage()));
+        return new ErrorDTO(e.getMessage());
     }
-
-    @ExceptionHandler(BadCredentialsException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDTO handleAuthenticationException(AuthenticationException e) {
-        return new ErrorDTO(List.of(e.getMessage()));
-    }
-
-    @ExceptionHandler(NotUniqueException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDTO handleNotUniqueException(NotUniqueException e) {
-        return new ErrorDTO(List.of(e.getMessage()));
-    }
-
 }
